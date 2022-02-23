54705bc016506c81cbc8c39f3f8201809cce4343
Kevin Bertrand <k.bertrand@desyntic.com>
2022-02-23 11:04:48 +0100

    Add PicturesView layout settings

---
+++ Instagrid/View/PicturesView.swift
@@ -0,0 +1,63 @@
+//
+//  PicturesView.swift
+//  Instagrid
+//
+//  Created by Kevin Bertrand on 23/02/2022.
+//
+
+import UIKit
+
+class PicturesView: UIView {
+    // MARK: Outlets
+    @IBOutlet weak var topLeftImageButton: UIButton!
+    @IBOutlet weak var topRightImageButton: UIButton!
+    @IBOutlet weak var bottomRightImageButton: UIButton!
+    @IBOutlet weak var bottomLeftImageButton: UIButton!
+    
+    
+    // MARK: Enumerations
+    enum Layout {
+        case oneImageBottom, oneImageTop, allFour
+    }
+    
+    // MARK: Properties
+    var layout: Layout = .allFour {
+        didSet {
+            changeGridLayout()
+        }
+    }
+    
+    // MARK: Methods
+    private func changeGridLayout() {
+        showAllImages()
+        switch layout {
+        case .allFour:
+            break
+        case .oneImageBottom:
+            bottomLeftImageButton.isHidden = true
+        case .oneImageTop:
+            topLeftImageButton.isHidden = true
+        }
+    }
+    
+    private func showAllImages() {
+        topLeftImageButton.isHidden = false
+        topRightImageButton.isHidden = false
+        bottomRightImageButton.isHidden = false
+        bottomLeftImageButton.isHidden = false
+    }
+    
+    func resetGrid() {
+        let defaultImage = UIImage(named: "Plus")
+        topLeftImageButton.setImage(defaultImage, for: .normal)
+        topRightImageButton.setImage(defaultImage, for: .normal)
+        bottomLeftImageButton.setImage(defaultImage, for: .normal)
+        bottomRightImageButton.setImage(defaultImage, for: .normal)
+        
+        layout = .allFour
+        
+        self.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.layer.shadowRadius = 6
+        self.layer.shadowOpacity = 0.2
+    }
+}
